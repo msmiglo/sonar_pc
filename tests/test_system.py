b633d766eec9d962de38506aa987987abf3a3dc9
Maciej Śmigielski <user@server.fake>
2026-02-13 02:40:16 +0100

    creating initial code files

---
+++ tests/test_system.py
@@ -0,0 +1,14 @@
+
+import unittest
+
+
+class TestWhole(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_xxx(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
